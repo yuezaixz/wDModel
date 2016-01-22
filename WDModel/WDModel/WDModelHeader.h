0e44c482ff291f382b366c956d25faa9f00b0262
yuezaixz <303178394@qq.com>
2016-01-22 17:14:15 +0800

    库创建

---
+++ WDModel/WDModel/WDModelHeader.h
@@ -0,0 +1,19 @@
+//
+//  WDModel.h
+//  WDModel
+//
+//  Created by 吴迪玮 on 16/1/22.
+//  Copyright © 2016年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WDModel.
+FOUNDATION_EXPORT double WDModelVersionNumber;
+
+//! Project version string for WDModel.
+FOUNDATION_EXPORT const unsigned char WDModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WDModel/PublicHeader.h>
+
+
